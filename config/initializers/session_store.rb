ead2e353cec793eada85508421af4d37c3dfac1d
Ben Birdsall & Robin Miller <student@epicodus.com>
2014-04-14 10:32:51 -0700

    Initial commit including Devise user authentication

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HeapOverflow::Application.config.session_store :cookie_store, key: '_heap_overflow_session'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+HeapOverflow::Application.routes.draw do
+  devise_for :users
+
+  resources :questions
+  root :to => 'questions#index'
+end
